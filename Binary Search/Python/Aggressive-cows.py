5e5ed20300c3ea7ae9f6646fd4ae6c8a91f60b22
Aditya Dengale <adityadengale16@gmail.com>
2025-10-19 17:29:43 +0530

    2 approaches

--- Binary Search/Python/Aggressive-cows.py
+++ Binary Search/Python/Aggressive-cows.py
@@ -1,4 +1,4 @@
-## brute force
+## brute force  O(n×maxDist) SC:O(1) 
 class Solution:
     def aggressiveCows(self, stalls, k):
         # code here
@@ -19,3 +19,31 @@ class Solution:
                 prev=a[i]
                 cnt = cnt+1;
         return cnt>=dist
+
+
+## optimal  solution TC:O(log maxDist) * O(n) SC:O(1)
+class Solution:
+    def aggressiveCows(self, stalls, k):
+        stalls.sort()
+        res=-1;
+        low=0 
+        high=stalls[-1]  - stalls[0]
+        while low<=high:
+            mid=low+(high-low)//2;
+            if self.check(stalls,mid,k):
+                res=mid
+                low=mid+1
+            else:
+                    high=mid-1
+        return res
+        
+    def check(self,a,dist,cows):
+        prev=a[0]
+        cnt=1
+        for i in range(1,len(a)):
+            if a[i]- prev >= dist:
+                cnt=cnt+1
+                prev=a[i]
+        return cnt>=cows
+        
+
