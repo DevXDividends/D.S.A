97e9f090bbc0e8dd77ad411a9a4b7554177bd7ea
Aditya Dengale <adityadengale16@gmail.com>
2025-10-19 17:24:08 +0530

    2 approaches

--- Binary Search/Aggressive-cows.cpp
+++ Binary Search/Aggressive-cows.cpp
@@ -1,3 +1,4 @@
+ //Brute force TC: O(n×maxDist) SC:O(1) 
 class Solution {
   public:
   bool check(vector<int> &a,int dist,int cows){
@@ -20,6 +21,37 @@ class Solution {
             if(check(stalls,i,k)){
                 res=i;
             }
+        }
+        return res;
+    }
+};
+
+// optimal solution TC:O(log maxDist) * O(n) SC:O(1)
+class Solution {
+  public:
+  bool check(vector<int> &a,int dist,int cows){
+      int cnt=1;
+      int prev=a[0];
+      for(int i=1;i<a.size();i++){
+          if(a[i]-prev>=dist){
+              cnt++;
+              prev=a[i];
+          }
+      }
+      return (cnt>=cows);
+  }
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        sort(stalls.begin(),stalls.end());
+        int res=-1;
+        
+        int low=0,high=stalls.back()-stalls[0];
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(stalls,mid,k)){
+                res=mid;
+                low=mid+1;
+            }else high=mid-1;
         }
         return res;
     }
