fb9428cc039a5f2e68773e95b138eb0fd53a7146
Aditya Dengale <adityadengale16@gmail.com>
2025-10-26 20:23:34 +0530

    2 approaches

--- Binary Search/Kth Smallest Product of Two Sorted Arrays.cpp
+++ Binary Search/Kth Smallest Product of Two Sorted Arrays.cpp
@@ -12,3 +12,42 @@ public:
         return products[k-1];
     }
 };
+// optimalTC:O(n1⋅logn2⋅log(max_product - min_product))  SC:O(1)
+class Solution {
+public:
+    long long checkLess(const vector<int>& a, const vector<int>& b,long long mid) {
+        long long cnt = 0;
+        int n = b.size();
+        for (int x : a) {
+            if (x > 0) {
+                long long target = (long long)floor((double)mid / x);
+
+                cnt += upper_bound(b.begin(), b.end(), target) - b.begin();
+
+            } else if (x < 0) {
+                cnt += b.end() - lower_bound(b.begin(), b.end(),
+                                             (long long)ceil((double)mid / x));
+            } else {
+                if (mid >= 0)
+                    cnt += n;
+            }
+        }
+        return cnt;
+    }
+    long long kthSmallestProduct(vector<int>& a, vector<int>& b, long long k) {
+        long long low = min({1LL * a[0] * b[0], 1LL * a[0] * b.back(),
+                             1LL * a.back() * b[0], 1LL * a.back() * b.back()});
+        long long high =
+            max({1LL * a[0] * b[0], 1LL * a[0] * b.back(),
+                 1LL * a.back() * b[0], 1LL * a.back() * b.back()});
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (checkLess(a, b, mid) >= k)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
