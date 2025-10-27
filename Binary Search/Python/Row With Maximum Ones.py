784b73b0593ef103063ac4949f6b7539b881b14d
Aditya Dengale <adityadengale16@gmail.com>
2025-10-27 23:48:58 +0530

    2 approaches

--- Binary Search/Python/Row With Maximum Ones.py
+++ Binary Search/Python/Row With Maximum Ones.py
@@ -12,3 +12,25 @@ class Solution:
                         maxCnt =cnt
                         ind = i
         return [ind,maxCnt]
+        
+# optimal (if each row is sorted in matrix) TC:O(rows * log n) SC:O(1)
+
+class Solution:
+    def bs(self,arr):
+        low = 0 
+        high=len(arr)-1
+        while low<=high:
+            mid  = low+(high-low)//2;
+            if arr[mid]:high = mid-1
+            else: low = mid+1
+        return low
+        
+    def rowWithMax1s(self, arr):
+        max_cnt = 0
+        idx = 0
+        for i in range(len(arr)):
+            cnt = len(arr[0]) - self.bs(arr[i])
+            if cnt>max_cnt:
+                max_cnt=cnt
+                idx= i 
+        return idx
