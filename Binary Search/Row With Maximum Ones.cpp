bff2bb84197f9f586c807ccc1ea71b4b937cbf96
Aditya Dengale <adityadengale16@gmail.com>
2025-10-27 23:40:38 +0530

    2 approaches
    Implemented binary search to find the row with maximum ones in a matrix.

--- Binary Search/Row With Maximum Ones.cpp
+++ Binary Search/Row With Maximum Ones.cpp
@@ -22,3 +22,32 @@ public:
         return {ind,maxCnt};
     }
 };
+
+// optimal (if each row is sorted in matrix) TC:O(rows * log n) SC:O(1)
+class Solution {
+  public:
+  int bs(vector<int> &a){
+      int low = 0,high=a.size()-1;
+      while(low<=high){
+          int mid= low+(high-low)/2;
+          if(a[mid]) high=mid-1;
+          else low=mid+1;
+      }
+      return low;
+  }
+  
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        int rows = arr.size();
+        int cols =arr[0].size();
+        int maxCnt = 0;
+        int idx = 0;
+        for(int i=0;i<rows;i++){
+            int cnt = cols  - bs(arr[i]);
+            if(cnt>maxCnt){
+                maxCnt=cnt;
+                idx = i;
+            }
+        }
+        return idx;
+    }
+};
