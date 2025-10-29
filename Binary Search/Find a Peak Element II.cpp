821fe1e83c923a56ae55a0afe675a5782098e526
Aditya Dengale <adityadengale16@gmail.com>
2025-10-29 14:53:51 +0530

    2 approaches

--- Binary Search/Find a Peak Element II.cpp
+++ Binary Search/Find a Peak Element II.cpp
@@ -20,3 +20,31 @@ public:
 
     }
 };
+// TC:O(rows *log cols) SC:O(1)
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        // in this soluiton we apply bs on colums
+        int rows = mat.size();
+        int cols = mat[0].size();
+        int low = 0, high = cols - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // calculate the max element of that row
+            int row = 0;
+            for (int i = 0; i < rows; i++)
+                if (mat[i][mid] > mat[row][mid])
+                    row = i;
+
+            // we have the max element of the col
+            // now we compare it with left and right values to check is there is
+            // a greater number on either side
+            int left = (mid - 1 >= 0) ? mat[row][mid - 1] : -1;
+            int right = (mid + 1 < cols) ? mat[row][mid + 1] : -1;
+            if(mat[row][mid]>left && mat[row][mid]>right) return {row,mid};
+            if(mat[row][mid] <right) low=mid+1;
+            else high=mid-1;
+        }
+        return {-1,-1};
+    }
+};
