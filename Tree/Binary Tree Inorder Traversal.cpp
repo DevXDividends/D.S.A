84e877cf37b312fc4b0fb9466053aac978668967
Aditya Dengale <adityadengale16@gmail.com>
2025-10-29 15:39:05 +0530

    .

---
+++ Tree/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,33 @@
+// TC:O(h) SC:(h) 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fun(TreeNode*& root, vector<int>& ans) {
+        // Inorder: Left->root->Right
+        if (!root)
+            return;
+        fun(root->left, ans);
+        ans.emplace_back(root->val);
+        fun(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (!root)
+            return {};
+        vector<int> ans;
+
+        fun(root, ans);
+
+        return ans;
+    }
+};
