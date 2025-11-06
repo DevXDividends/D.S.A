58a8bcc8de02f027b4634a113f29b0c8a7701f3e
Aditya Dengale <adityadengale16@gmail.com>
2025-11-06 23:54:51 +0530

    .

---
+++ Linked List/Odd Even Linked List.cpp
@@ -0,0 +1,28 @@
+// TC:O(n) SC:O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) return nullptr;
+        ListNode*odd=head;
+        ListNode* even=head->next;
+        ListNode* conHead=head->next;
+        while(even && even->next){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
+        }
+        odd->next=conHead;
+        return head;
+    }
+};
