08120a693aac5e589a7d9efcb74732b084be4e6f
Aditya Dengale <adityadengale16@gmail.com>
2025-11-10 21:04:56 +0530

    2 approaches

--- Binary Search/K-diff Pairs in an Array.cpp
+++ Binary Search/K-diff Pairs in an Array.cpp
@@ -20,3 +20,27 @@ public:
         return maxCnt;
     }
 };
+// TC:O(n log n) SC:O(1)
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int maxCnt=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1])continue;
+            // find the complement 
+            long long comp=nums[i]+k;
+            int low=i+1,high=n-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(nums[mid]==comp){
+                    maxCnt++;
+                    break;
+                }else if(nums[mid]>comp)high=mid-1;
+                else low=mid+1;
+            }
+        }
+        return maxCnt;
+    }
+};
