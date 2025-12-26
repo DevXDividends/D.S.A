22abfca02cac3d380d13b26330e79483306dd8b6
Aditya Dengale <adityadengale16@gmail.com>
2025-12-26 22:13:19 +0530

    2 approaches
    Added an alternative method to check if a number is a power of three using a mathematical approach.

---
+++ Recursion/Power of Three.cpp
@@ -0,0 +1,20 @@
+TC:O(log n) SC:O(log n)
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+        if (n % 3 != 0)
+            return false;
+        return isPowerOfThree(n / 3);
+    }
+};
+// TC:O(1) SC:O(1)
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+};
