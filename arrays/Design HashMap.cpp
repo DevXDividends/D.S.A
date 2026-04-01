c1d6481e2f661cf9d9c547df4c4145ffd8c52ab2
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-04-01 12:39:30 +0530

    Add MyHashMap class with basic operations
    Implement a basic hash map with put, get, and remove methods.

---
+++ arrays/Design HashMap.cpp
@@ -0,0 +1,29 @@
+// TC:O(1) SC:O(n) 
+class MyHashMap {
+    vector<int> map;
+
+public:
+    MyHashMap() {
+        map = vector<int>(1000001,-1);
+    }
+    
+    void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    int get(int key) {
+        return map[key];
+    }
+    
+    void remove(int key) {
+        map[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
