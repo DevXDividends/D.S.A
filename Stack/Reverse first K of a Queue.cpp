78d6e0276bef8cb71a9cf4cdb722b23e6238e973
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-04-22 22:34:17 +0530

    stack

---
+++ Stack/Reverse first K of a Queue.cpp
@@ -0,0 +1,26 @@
+// TC:O(n) SC:O(K)
+class Solution {
+  public:
+    queue<int> reverseFirstK(queue<int> q, int k) {
+        // code here
+        
+        int  n  = q.size();
+        if(k>n)return q;
+        stack<int> st;
+        for(int i=0;i<k;i++){
+            st.push(q.front());
+            q.pop();
+        }
+        int n2 =q.size();
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        for(int i=0;i<n2;i++){
+            q.push(q.front());
+            q.pop();
+        }
+        return q;
+        
+    }
+};
