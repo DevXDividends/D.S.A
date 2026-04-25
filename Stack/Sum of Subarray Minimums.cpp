dab60ed6f5ac3cefb4aa51495545f138a9fd2634
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-04-25 17:31:23 +0530

    3 approaches

--- Stack/Sum of Subarray Minimums.cpp
+++ Stack/Sum of Subarray Minimums.cpp
@@ -54,3 +54,34 @@ public:
         return sum % MOD;
     }
 };
+// optimal TCL:O(n) + O(n) + O(n) = O(n)
+// SC:O(n) + O(n) + O(n) = O(n)
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int mod = 1e9 + 7;
+        vector<int> left(n), right(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+
+            left[i] = st.empty() ? (i + 1) : (i - st.top());
+            st.push(i);
+        }
+        while (!st.empty())
+            st.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            right[i] = st.empty() ? (n - i) : (st.top() - i);
+            st.push(i);
+        }
+
+        long long total = 0;
+        for (int i = 0; i < n; i++)
+            total = (total + (long long )arr[i] * left[i] * right[i]) % mod;
+        return total;
+    }
+};
