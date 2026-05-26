76dfad702d18d4e231fc89cca121e3d138fafbc7
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-05-26 22:15:11 +0530

    3 approaches

--- Stack/Largest Rectangle in Histogram.cpp
+++ Stack/Largest Rectangle in Histogram.cpp
@@ -64,3 +64,30 @@ public:
         return maxArea;
     }
 };
+// optimal TC:O(n) SC:O(n) 
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> stk;
+        int maxArea = 0;
+        int n = heights.size();
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && heights[stk.top()] > heights[i]) {
+                int element = stk.top();
+                stk.pop();
+                int nse = i;
+                int pse = stk.empty() ? -1 : stk.top();
+                maxArea = max(maxArea, heights[element] * (nse - pse - 1));
+            }
+            stk.push(i);
+        }
+        while (!stk.empty()) {
+            int element = stk.top();
+                stk.pop();
+                int nse = n;
+                int pse = stk.empty() ? -1 : stk.top();
+                maxArea = max(maxArea, heights[element] * (nse - pse - 1));
+        }
+        return maxArea;
+    }
+};
