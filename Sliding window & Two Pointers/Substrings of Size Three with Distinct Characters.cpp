d746eed6aa5dfcb339d8ce1d8b00df639a22ab89
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-07-01 23:12:36 +0530

    variable window

--- Sliding window & Two Pointers/Substrings of Size Three with Distinct Characters.cpp
+++ Sliding window & Two Pointers/Substrings of Size Three with Distinct Characters.cpp
@@ -20,3 +20,31 @@ public:
         return cnt;
     }
 };
+// TC:O(n) SC:O(1)
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int n = s.length();
+        int cnt = 0;
+        int l = 0, r = 0;
+        unordered_map<char,int> mpp;
+        while (r < n) {
+            
+                mpp[s[r]]++;
+            while ( r-l+1>3) {
+                mpp[s[l]]--;
+                if(mpp[s[l]]==0)
+                mpp.erase(s[l]);
+                
+                l++;
+            }
+
+
+            if (r-l+1 == 3 && mpp.size()==3)
+            
+                cnt++;
+            r++;
+        }
+        return cnt;
+    }
+};
