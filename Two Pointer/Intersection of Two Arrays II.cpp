be1ef150a11bea9726548a46ecc3e3c8080eee36
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-07-04 15:50:35 +0530

    Pattern 3: Two Arrays Traversal

---
+++ Two Pointer/Intersection of Two Arrays II.cpp
@@ -0,0 +1,23 @@
+// TC:O(n log n) + (n) SC:O(1)
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
+        vector<int> ans;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        while (i < n1 && j < n2) {
+            if (nums1[i] == nums2[j]) {
+                ans.emplace_back(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
+        }
+        return ans;
+    }
+};
