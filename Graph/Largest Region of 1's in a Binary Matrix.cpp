86d32aed2d1f764013d5ef419f37a3af7cd9b929
Aditya Uday Dengale <adityadengale16@gmail.com>
2026-08-02 20:29:14 +0530

    Pattern 1: DFS - Depth-First Search (Connectivity, Components, Ordering)

---
+++ Graph/Largest Region of 1's in a Binary Matrix.cpp
@@ -0,0 +1,34 @@
+// TC:O(m x n) SC:O(m x n)
+class Solution {
+  public:
+    void dfs(int row,int col,vector<vector<int>>& grid,vector<vector<bool>> &vis,int &currArea){
+        if(row<0 || col<0 || row>=grid.size() || col>=grid[0].size() || vis[row][col])
+        return;
+        if(!grid[row][col])
+        return;
+        vis[row][col] = true;
+        currArea++;
+        int dr[]={-1,-1,-1,0,1,1,1,0};
+        int dc[] = {-1,0,1,1,1,0,-1,-1};
+        for(int i=0;i<8;i++){
+            dfs(row + dr[i],col + dc[i],grid,vis,currArea);
+        }
+    }
+    int findMaxArea(vector<vector<int>>& grid) {
+        // Code here
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<bool>> vis(row,vector<bool>(col,false));
+        int maxArea =0;
+        for(int r = 0 ;r<row;r++){
+            for(int c = 0;c<col;c++){
+                if(grid[r][c] && !vis[r][c]){
+                    int currArea = 0;
+                    dfs(r,c,grid,vis,currArea);
+                    maxArea =max(maxArea,currArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+};
